5d69c1f566ec429a4daa757c5c790edf32f71866
vincentBaylly <vincent.baylly@gmail.com>
2015-01-13 13:13:32 -0500

    update jaxb annotation

--- src/main/java/com/rink/xml/jaxb/model/Rinks.java
+++ src/main/java/com/rink/xml/jaxb/model/Rinks.java
@@ -3,6 +3,8 @@ package com.rink.xml.jaxb.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
@@ -10,8 +12,11 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import com.rink.xml.jaxb.adapter.RinkAdapter;
 
 @XmlRootElement(name="patinoires")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Rinks{
 	
+	@XmlElement(type=Rink.class, name="patinoire")
+    @XmlJavaTypeAdapter(RinkAdapter.class)
     private List<Rink> rinkList;
     
     public Rinks() {
@@ -22,10 +27,12 @@ public class Rinks{
         this.rinkList = rinkList;
     }
     
-    @XmlElement(type=Rink.class, name="patinoire")
-    @XmlJavaTypeAdapter(RinkAdapter.class)
 	public List<Rink> getRinkList() {
 		return rinkList;
 	}
-
+	
+    public void setRinkList(List<Rink> rinkList) {
+        this.rinkList = rinkList;
+    }
+	
 }
