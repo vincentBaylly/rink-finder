a53446ac838e13a5f7839b31712cee68d1c2c02f
vincentBaylly <vincent.baylly@gmail.com>
2015-01-28 14:07:49 -0500

    Update Frame.java

--- src/main/java/com/rink/window/Frame.java
+++ src/main/java/com/rink/window/Frame.java
@@ -3,20 +3,17 @@ package com.rink.window;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Collections;
 import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -26,53 +23,53 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.rink.utils.ComparatorForRink;
+import com.rink.app.Treatment;
 import com.rink.utils.RinkUtils;
 import com.rink.xml.jaxb.model.Borough;
 import com.rink.xml.jaxb.model.Rink;
 import com.rink.xml.jaxb.model.Rinks;
 
-@SuppressWarnings("serial")
-public class Frame extends JFrame {
+@SuppressWarnings({"serial", "rawtypes"})
+public class RinkFrame extends JFrame {
 
-	private static final Log LOG = LogFactory.getLog(Frame.class);
-
-	private static final String MTL_WEBSITE = "www2.ville.montreal.qc.ca";
-
-	private static final String RINKS_XML = "src/main/resources/rinks-jaxb.xml";
-
-	private static final String RINKS_XML_URL = "http://www2.ville.montreal.qc.ca/services_citoyens/pdf_transfert/L29_PATINOIRE.xml";
-
-	private static final ComparatorForRink comparatorForRink = new ComparatorForRink();
-
+	private static final Log LOG = LogFactory.getLog(RinkFrame.class);
+	
+	private Treatment treatment;
+	
 	private JMenuItem exitMenuItem;
 	private JMenu fileMenu;
+	private JPanel listPanel;
+	private JPanel buttonPanel;
 	private JButton boroughButton;
 	private JButton condButton;
 	private JLabel handNameOut;
 	private JMenuBar menuBar;
 	private JScrollPane scrollPane;
+	private JComboBox boroughList;
 
-	/**
-	 * Creates new form Frame
-	 */
-	public Frame() {
-		initComponents();
+	public Treatment getTreatment() {
+		return treatment;
 	}
 
+	public void setTreatment(Treatment treatment) {
+		this.treatment = treatment;
+	}
+	
 	/**
 	 * This method is called from within the constructor to initialize the form.
 	 */
+	@SuppressWarnings("unused")
 	private void initComponents() {
-
+		
+		setPreferredSize(new Dimension(600, 400));
+		
+        boroughList = new JComboBox();
+		buttonPanel = new JPanel();
+		listPanel = new JPanel();
 		boroughButton = new JButton();
 		condButton = new JButton();
 		handNameOut = new JLabel();
@@ -80,11 +77,18 @@ public class Frame extends JFrame {
 		fileMenu = new JMenu();
 		exitMenuItem = new JMenuItem();
 		scrollPane = new JScrollPane(handNameOut, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-//		scrollPane.setVisible(false);
-
+		scrollPane.setPreferredSize(new Dimension(400, 285));
+		listPanel.setPreferredSize(new Dimension(400, 300));
+		listPanel.add(scrollPane);
+		
+		getContentPane().setLayout(new BorderLayout());
+		
+		fillListCombo();
+		
 		Container con = getContentPane();
 		con.setBackground(new java.awt.Color(0, 0, 51));
-
+		buttonPanel.setBackground(new java.awt.Color(0, 0, 51));
+		listPanel.setBackground(new java.awt.Color(0, 0, 51));
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 		boroughButton.setText("Sort by Borough");
@@ -98,6 +102,12 @@ public class Frame extends JFrame {
 		condButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				condButtonActionPerformed(evt);
+			}
+		});
+		
+		boroughList.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				boroughListActionPerformed(evt);
 			}
 		});
 		
@@ -117,17 +127,28 @@ public class Frame extends JFrame {
 
 		setJMenuBar(menuBar);
 		
-		JPanel panel = new JPanel();
+		buttonPanel.add(BorderLayout.EAST, boroughButton);
+		buttonPanel.add(BorderLayout.WEST, condButton);
 		
-		panel.add(BorderLayout.EAST, boroughButton);
-		panel.add(BorderLayout.WEST, condButton);
+		getContentPane().add(BorderLayout.NORTH, boroughList);
+		getContentPane().add(BorderLayout.CENTER, listPanel);
+		getContentPane().add(BorderLayout.SOUTH, buttonPanel);
 		
-		getContentPane().add(BorderLayout.CENTER, scrollPane);
-		getContentPane().add(BorderLayout.SOUTH, panel);
-		
-		pack();
+        setVisible(true);
+        setState(RinkFrame.NORMAL);
+        
+        pack();
 	}
-
+	
+	@SuppressWarnings("unchecked")
+	private void fillListCombo(){
+		
+		for(Borough borough : treatment.getAvailableBorough()){
+			boroughList.addItem(borough);
+		}
+		
+	}
+	
 	/**
 	 * exit menu catch event
 	 * 
@@ -144,15 +165,14 @@ public class Frame extends JFrame {
 	 */
 	private void boroughButtonActionPerformed(ActionEvent evt) {
 
-		Rinks result = unmarshallFlux("borough");
+		Rinks result = treatment.getRinks("borough");
 		RinkUtils.printRinks(result);
 		handNameOut.setText("");
 		ImageIcon imageIcon = new ImageIcon(append(result.getRinkList()));
 		handNameOut.setIcon(imageIcon);
 		handNameOut.setHorizontalAlignment(SwingConstants.CENTER);
-//		scrollPane.setVisible(true);
 		
-		LOG.info(result);
+		LOG.debug(result);
 	}
 	
 	/**
@@ -161,60 +181,38 @@ public class Frame extends JFrame {
 	 * @param evt
 	 */
 	private void condButtonActionPerformed(ActionEvent evt) {
-
-		Rinks result = unmarshallFlux("condition");
+		
+		Rinks result = treatment.getRinks("condition");
 		RinkUtils.printRinks(result);
 		handNameOut.setText("");
 		ImageIcon imageIcon = new ImageIcon(append(result.getRinkList()));
 		handNameOut.setIcon(imageIcon);
 		handNameOut.setHorizontalAlignment(SwingConstants.CENTER);
-//		scrollPane.setVisible(true);
 		
-		LOG.info(result);
+		LOG.debug(result);
+		
+	}
+
+	/**
+	 * find rinks from borough
+	 * sort by condition
+	 * @param evt
+	 */
+	private void boroughListActionPerformed(ActionEvent evt) {
+		
+		JComboBox cb = (JComboBox)evt.getSource();
+        Borough borough = (Borough)cb.getSelectedItem();
+		Rinks result = treatment.getRinksFromBorough(borough.getKey());
+		RinkUtils.printRinks(result);
+		handNameOut.setText("");
+		ImageIcon imageIcon = new ImageIcon(append(result.getRinkList()));
+		handNameOut.setIcon(imageIcon);
+		handNameOut.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		LOG.debug(result);
+
 	}
 	
-	public static Rinks unmarshallFlux(String sortType) {
-
-		Rinks rinks = null;
-
-		try {
-			// create JAXB context and instantiate marshaller
-			JAXBContext context = null;
-			context = JAXBContext.newInstance(Rinks.class, Rink.class, Borough.class);
-
-			Unmarshaller unmarshaller = context.createUnmarshaller();
-			
-			Marshaller marshaller = context.createMarshaller();
-			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-
-			if (RinkUtils.isInternetReachable(MTL_WEBSITE)) {
-
-				URL u = new URL(RINKS_XML_URL);
-				InputStream in = u.openStream();
-				rinks = (Rinks) unmarshaller.unmarshal(in);
-
-				// Write to File
-				marshaller.marshal(rinks, new FileOutputStream(RINKS_XML));
-			} else {
-
-				rinks = (Rinks) unmarshaller.unmarshal(new File(RINKS_XML));
-			}
-			
-			if("borough".equalsIgnoreCase(sortType)){
-				Collections.sort(rinks.getRinkList(), comparatorForRink.new BoroughComparator());
-			}else if("condition".equalsIgnoreCase(sortType)){
-				Collections.sort(rinks.getRinkList(), comparatorForRink.new RinkConditionComparator());
-			}
-
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return rinks;
-	}
-
 	/**
 	 * add an icon on rink
 	 * 
@@ -223,32 +221,31 @@ public class Frame extends JFrame {
 	 */
 	public static Image append(List<Rink> rinksResult) {
 
-		BufferedImage buf = null;
+		BufferedImage bImage = null;
 		
 		if (rinksResult.get(0) != null && rinksResult.get(0).getImageIcon() != null) {
-			int wMax = 500;
-			int hMax = 20000;
+			int wMax = 400;
 			int h1 = 0;
-			buf = new BufferedImage(wMax, hMax, BufferedImage.TYPE_INT_ARGB);
+			bImage = new BufferedImage(wMax, 56 * rinksResult.size(), BufferedImage.TYPE_INT_ARGB);
 			
 			for (Rink rink : rinksResult) {
 				if (rink != null) {
 
-					Graphics2D g2 = buf.createGraphics();
+					Graphics2D g2 = bImage.createGraphics();
 					g2.drawImage(rink.getImageIcon().getImage(), 0, h1, null);
 					g2.setColor(Color.red);
 					g2.drawLine(0, h1, rink.getImageIcon().getImage().getWidth(null) + 300, h1);
 					g2.drawString(rink.getName(), rink.getImageIcon().getImage().getWidth(null) + 10, h1 + 15);
 					g2.drawString(rink.getBorough().getName(), rink.getImageIcon().getImage().getWidth(null) + 10, h1 + 30);
 					g2.drawString(rink.getCondition(), rink.getImageIcon().getImage().getWidth(null) + 10, h1 + 45);
-					h1 += rink.getImageIcon().getImage().getWidth(null) + 20;
+					h1 += rink.getImageIcon().getImage().getHeight(null) + 20;
 
 				}
 			}
-
+			
 		}
-
-		return buf;
+		
+		return bImage;
 	}
 
 }
